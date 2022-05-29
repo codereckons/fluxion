b657d9e7cbf55ce0ffcdab56b803826eb862e63d
Joel Falcou <joel.falcou@lri.fr>
2022-05-29 12:21:35 +0200

    More infrastructure elements

---
+++ doc/index.hpp
@@ -0,0 +1,41 @@
+#error DO NOT INCLUDE - DOCUMENTATION PURPOSE ONLY
+
+//==================================================================================================
+//! \mainpage The C++20 Compact Tuple Tools
+//!
+//! **Fluxion** is C++20 implementation of a tuple-like class. It tries to be as close to
+//! `std::tuple` as possible but also wants to compile faster, uses a better C++20 oriented interface,
+//! and new features like:
+//!
+//!   -  a fast to compile tuple implementation
+//!   -  quality of life improvement over the standard tuple implementation
+//!   -  a protocol to adapt user-defined type to act as tuples
+//!   -  algorithm on tuples
+//!
+//! # A Short Example
+//!
+//!
+//! # Licence
+//!
+//! This library is licensed under the [MIT License](http://opensource.org/licenses/MIT):
+//!
+//! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ none
+//! Copyright : FLUXION Contributors & Maintainers
+//!
+//! Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+//! and associated documentation files (the “Software”), to deal in the Software without restriction,
+//! including without limitation the rights to use, copy, modify, merge, publish, distribute,
+//! sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+//! furnished to do so, subject to the following conditions:
+//!
+//! The above copyright notice and this permission notice shall be included in all copies or substantial
+//! portions of the Software.
+//!
+//! THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+//! NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//! IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//! CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//!
+//==================================================================================================
