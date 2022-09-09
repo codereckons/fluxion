6e20872051efe769744c8fd4532cf8196741356f
jtlap <jtlapreste@gmail.com>
2022-09-09 18:39:30 +0200

    sph_j1

--- include/flx/differential/module/bessel/sph_bessel_j1.hpp
+++ include/flx/differential/module/bessel/sph_bessel_j1.hpp
@@ -18,13 +18,13 @@ namespace eve::detail
     if constexpr(has_native_abi_v<T>)
     {
       //−2sin(x)/x^3+2cos(x)/x^2+sin(x)/x
-      auto [s, c] = sincos(x);
+//      auto [s, c] = sincos(x);
       auto sq =  sqr(x);
-      return if_else(x == inf(as(x))
+      return if_else(sq == inf(as(x))//not so good small not to small values
                     , zero
                     , if_else(abs(x) < eps(as(x))
-                             , inf(as(x))
-                             , fma(sq-2, s, 2*x*c)/(sq*x)
+                             , third(as(x))+ 3*sq/20
+                             , eve::sph_bessel_j0(x)-2*eve::sph_bessel_j1(x)/x
                              )
                     );
     }
--- test/unit/module/bessel/sph_bessel_j1.cpp
+++ test/unit/module/bessel/sph_bessel_j1.cpp
@@ -6,6 +6,7 @@
 #include "test.hpp"
 #include <flx/differential/bessel.hpp>
 #include <boost/math/special_functions/bessel_prime.hpp>
+#include <iomanip>
 
 //==================================================================================================
 // Types tests
@@ -14,14 +15,9 @@ TTS_CASE_TPL("Check return types of eve::sph_bessel_j1", flx::test::simd::ieee_r
 <typename T>(tts::type<T>)
 {
   using v_t = eve::element_type_t<T>;
-  using i_t = eve::as_integer_t<v_t>;
-  using I_t = eve::wide<i_t, eve::cardinal_t<T>>;
-  TTS_EXPR_IS(flx::diff(eve::sph_bessel_j1)(T(), T()), T);
-  TTS_EXPR_IS(flx::diff(eve::sph_bessel_j1)(v_t(), v_t()), v_t);
-  TTS_EXPR_IS(flx::diff(eve::sph_bessel_j1)(i_t(), T()), T);
-  TTS_EXPR_IS(flx::diff(eve::sph_bessel_j1)(I_t(), T()), T);
-  TTS_EXPR_IS(flx::diff(eve::sph_bessel_j1)(i_t(), v_t()), v_t);
-  TTS_EXPR_IS(flx::diff(eve::sph_bessel_j1)(I_t(), v_t()), T);
+
+  TTS_EXPR_IS(flx::diff(eve::sph_bessel_j1)(T()), T);
+  TTS_EXPR_IS(flx::diff(eve::sph_bessel_j1)(v_t()), v_t);
 };
 
 //==================================================================================================
@@ -29,15 +25,18 @@ TTS_CASE_TPL("Check return types of eve::sph_bessel_j1", flx::test::simd::ieee_r
 //==================================================================================================
 TTS_CASE_WITH("Check behavior of eve::sph_bessel_j1(eve::wide)",
               flx::test::simd::ieee_reals,
-              tts::generate(tts::ramp(0)
-                          , tts::randoms(0.0,10.0)
+              tts::generate(tts::randoms(0.1,10.0)
                            )
               )
-<typename I,typename T>(I const & i,T const& a0, T const & a1)
+<typename T>(T const& a0)
 {
   using eve::sph_bessel_j1;
   using eve::detail::map;
+  using v_t = eve::element_type_t<T>;
 
-  auto dsph_bessel_j1 = [&](auto i, auto e, auto f) { return boost::math::sph_bessel_j1_prime(i, e, f); };
-  TTS_ULP_EQUAL(flx::diff_1st(eve::sph_bessel_j1)(i, a0, a1), map(dsph_bessel_j1, a0, a1), 0.5);
+  auto dsph_bessel_j1 = [&](auto e) ->v_t{ return boost::math::sph_bessel_prime(1, double(e)); };
+  TTS_ULP_EQUAL(flx::diff_1st(eve::sph_bessel_j1)(a0), map(dsph_bessel_j1, a0), 50)
+    << "-> " << std::setprecision(15) << a0 << '\n'
+    << "-> " << "flx   " << flx::diff_1st(eve::sph_bessel_j1)(a0)<< '\n'
+    << "-> " << "boost " << map(dsph_bessel_j1, a0)<< '\n';
 };
