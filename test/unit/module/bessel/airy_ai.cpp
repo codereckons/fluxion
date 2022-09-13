becc5aca14febd1f0cee3bc5e6ac9e655a1bf8cb
jtlap <jtlapreste@gmail.com>
2022-09-13 09:25:47 +0200

    airy

--- test/unit/module/bessel/airy_ai.cpp
+++ test/unit/module/bessel/airy_ai.cpp
@@ -5,7 +5,7 @@
 //================================================================================================== //!
 #include "test.hpp"
 #include <flx/differential/bessel.hpp>
-#include <boost/math/special_functions/bessel_prime.hpp>
+#include <boost/math/special_functions/airy.hpp>
 
 //==================================================================================================
 // Types tests
@@ -14,14 +14,8 @@ TTS_CASE_TPL("Check return types of eve::airy_ai", flx::test::simd::ieee_reals)
 <typename T>(tts::type<T>)
 {
   using v_t = eve::element_type_t<T>;
-  using i_t = eve::as_integer_t<v_t>;
-  using I_t = eve::wide<i_t, eve::cardinal_t<T>>;
-  TTS_EXPR_IS(flx::diff(eve::airy_ai)(T(), T()), T);
-  TTS_EXPR_IS(flx::diff(eve::airy_ai)(v_t(), v_t()), v_t);
-  TTS_EXPR_IS(flx::diff(eve::airy_ai)(i_t(), T()), T);
-  TTS_EXPR_IS(flx::diff(eve::airy_ai)(I_t(), T()), T);
-  TTS_EXPR_IS(flx::diff(eve::airy_ai)(i_t(), v_t()), v_t);
-  TTS_EXPR_IS(flx::diff(eve::airy_ai)(I_t(), v_t()), T);
+  TTS_EXPR_IS(flx::diff(eve::airy_ai)(T()), T);
+  TTS_EXPR_IS(flx::diff(eve::airy_ai)(v_t()), v_t);
 };
 
 //==================================================================================================
@@ -29,15 +23,16 @@ TTS_CASE_TPL("Check return types of eve::airy_ai", flx::test::simd::ieee_reals)
 //==================================================================================================
 TTS_CASE_WITH("Check behavior of eve::airy_ai(eve::wide)",
               flx::test::simd::ieee_reals,
-              tts::generate(tts::ramp(0)
-                          , tts::randoms(0.0,10.0)
+              tts::generate( tts::randoms(0.0,10.0),
+                             tts::randoms(-10.0,0.0)
                            )
               )
-<typename I,typename T>(I const & i,T const& a0, T const & a1)
+<typename T>(T const& a0, T const& a1)
 {
   using eve::airy_ai;
   using eve::detail::map;
 
-  auto dairy_ai = [&](auto i, auto e, auto f) { return boost::math::airy_ai_prime(i, e, f); };
-  TTS_ULP_EQUAL(flx::diff_1st(eve::airy_ai)(i, a0, a1), map(dairy_ai, a0, a1), 0.5);
+  auto dairy_ai = [&](auto e) { return boost::math::airy_ai_prime(e); };
+  TTS_ULP_EQUAL(flx::diff_1st(eve::airy_ai)(a0), map(dairy_ai, a0), 50);
+  TTS_ULP_EQUAL(flx::diff_1st(eve::airy_ai)(a1), map(dairy_ai, a1), 50);
 };
--- test/unit/module/bessel/airy_bi.cpp
+++ test/unit/module/bessel/airy_bi.cpp
@@ -5,7 +5,7 @@
 //================================================================================================== //!
 #include "test.hpp"
 #include <flx/differential/bessel.hpp>
-#include <boost/math/special_functions/bessel_prime.hpp>
+#include <boost/math/special_functions/airy.hpp>
 
 //==================================================================================================
 // Types tests
@@ -14,14 +14,8 @@ TTS_CASE_TPL("Check return types of eve::airy_bi", flx::test::simd::ieee_reals)
 <typename T>(tts::type<T>)
 {
   using v_t = eve::element_type_t<T>;
-  using i_t = eve::as_integer_t<v_t>;
-  using I_t = eve::wide<i_t, eve::cardinal_t<T>>;
-  TTS_EXPR_IS(flx::diff(eve::airy_bi)(T(), T()), T);
-  TTS_EXPR_IS(flx::diff(eve::airy_bi)(v_t(), v_t()), v_t);
-  TTS_EXPR_IS(flx::diff(eve::airy_bi)(i_t(), T()), T);
-  TTS_EXPR_IS(flx::diff(eve::airy_bi)(I_t(), T()), T);
-  TTS_EXPR_IS(flx::diff(eve::airy_bi)(i_t(), v_t()), v_t);
-  TTS_EXPR_IS(flx::diff(eve::airy_bi)(I_t(), v_t()), T);
+  TTS_EXPR_IS(flx::diff(eve::airy_bi)(T()), T);
+  TTS_EXPR_IS(flx::diff(eve::airy_bi)(v_t()), v_t);
 };
 
 //==================================================================================================
@@ -29,15 +23,16 @@ TTS_CASE_TPL("Check return types of eve::airy_bi", flx::test::simd::ieee_reals)
 //==================================================================================================
 TTS_CASE_WITH("Check behavior of eve::airy_bi(eve::wide)",
               flx::test::simd::ieee_reals,
-              tts::generate(tts::ramp(0)
-                          , tts::randoms(0.0,10.0)
+              tts::generate( tts::randoms(0.0,10.0),
+                             tts::randoms(-10.0,0.0)
                            )
               )
-<typename I,typename T>(I const & i,T const& a0, T const & a1)
+<typename T>(T const& a0, T const& a1)
 {
   using eve::airy_bi;
   using eve::detail::map;
 
-  auto dairy_bi = [&](auto i, auto e, auto f) { return boost::math::airy_bi_prime(i, e, f); };
-  TTS_ULP_EQUAL(flx::diff_1st(eve::airy_bi)(i, a0, a1), map(dairy_bi, a0, a1), 0.5);
+  auto dairy_bi = [&](auto e) { return boost::math::airy_bi_prime(e); };
+  TTS_ULP_EQUAL(flx::diff_1st(eve::airy_bi)(a0), map(dairy_bi, a0), 50);
+  TTS_ULP_EQUAL(flx::diff_1st(eve::airy_bi)(a1), map(dairy_bi, a1), 50);
 };
