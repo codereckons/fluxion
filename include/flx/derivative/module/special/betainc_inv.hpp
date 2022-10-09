82daa7aa0abede043c356d2c0756e230d97fe427
jtlap <jtlapreste@gmail.com>
2022-10-09 16:18:39 +0200

    betainc_inv

--- include/flx/derivative/module/special/betainc_inv.hpp
+++ include/flx/derivative/module/special/betainc_inv.hpp
@@ -13,22 +13,23 @@ namespace eve::detail
   EVE_FORCEINLINE  auto betainc_inv_(EVE_SUPPORTS(cpu_)
                               , flx::derivative_type<N> const &
                               , S const &s
-                              , T const &a
-                              , U const &b) noexcept
-  requires compatible_values<T, U> && compatible_values<T, S>
-  {
-    return arithmetic_call(flx::derivative_type<N>{}(betainc_inv), s, a, b);
-  }
-
-  template<floating_real_value T>
-  EVE_FORCEINLINE constexpr auto betainc_inv_(EVE_SUPPORTS(cpu_)
-                                   , flx::derivative_type<1> const &
-                                   , T const &s
-                                   , T const &x
-                                   , T const &y) noexcept
-  requires(has_native_abi_v<T>)
+                              , T const &x
+                              , U const &y) noexcept
   {
     auto b = betainc_inv(s, x, y);
     return rec(flx::derivative_1st(betainc)(b, x, y));
+//    return arithmetic_call(flx::derivative_type<N>{}(betainc_inv), s, a, b);
   }
+
+//   template<floating_real_value T>
+//   EVE_FORCEINLINE constexpr auto betainc_inv_(EVE_SUPPORTS(cpu_)
+//                                    , flx::derivative_type<1> const &
+//                                    , T const &s
+//                                    , T const &x
+//                                    , T const &y) noexcept
+//   requires(has_native_abi_v<T>)
+//   {
+//     auto b = betainc_inv(s, x, y);
+//     return rec(flx::derivative_1st(betainc)(b, x, y));
+//   }
 }
