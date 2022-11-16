09346aac2cb71071013bd6f75b32c72b7f621149
Joel Falcou <joel.falcou@lri.fr>
2022-11-16 10:51:08 +0100

    Enable CI

---
+++ .github/actions/run_docker/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+set -e
+echo "::group:: Running: 'cmake .. -G Ninja -DCMAKE_CXX_FLAGS="$1" $2'"
+mkdir build
+cd build
+cmake .. -G Ninja -DFLUXION_OPTIONS="$1" $2
+echo "::endgroup::"
+
+echo "::group:: Compiling Unit Tests"
+ninja -v unit -j $3
+echo "::endgroup::"
+
+echo "::group:: Running Unit Tests"
+ctest --output-on-failure -j $3
+echo "::endgroup::"
+
+exit 0
