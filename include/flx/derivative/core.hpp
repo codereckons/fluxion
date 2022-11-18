7fab33221b8770cac737e9a3078084c200cf8e1c
jtlap <jtlapreste@gmail.com>
2022-11-18 18:02:25 +0100

    lerp

--- include/flx/derivative/core.hpp
+++ include/flx/derivative/core.hpp
@@ -27,6 +27,7 @@
 #include <flx/derivative/module/core/fsm.hpp>
 #include <flx/derivative/module/core/fsnm.hpp>
 #include <flx/derivative/module/core/inc.hpp>
+#include <flx/derivative/module/core/lerp.hpp>
 #include <flx/derivative/module/core/manhattan.hpp>
 #include <flx/derivative/module/core/maxabs.hpp>
 #include <flx/derivative/module/core/max.hpp>
