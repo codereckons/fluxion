032ab90580a3ec83f1c87c77ea8d296181fbb2a8
jtlap <jtlapreste@gmail.com>
2022-11-24 07:41:15 +0100

    adding to lpnorm derivative relative to p

--- test/unit/module/derivative/math/lpnorm.cpp
+++ test/unit/module/derivative/math/lpnorm.cpp
@@ -32,9 +32,15 @@ TTS_CASE_WITH("Check behavior of eve::lpnorm(eve::wide)",
 {
   using eve::lpnorm;
   using eve::detail::map;
+  using eve::pow_abs;
+  using eve::log_abs;
+  auto dlpnorm1 = [&](auto p, auto x, auto y) { auto h = eve::lpnorm(p, x, y);
+                               return h*((pow_abs(x, p)*log_abs(x) + pow_abs(y, p)*log_abs(y))/(p*(pow_abs(x, p)+pow_abs(y, p)))
+                                                          - log_abs(pow_abs(x, p)+pow_abs(y, p))/eve::sqr(p)); };
+  auto dlpnorm2 = [&](auto p, auto x, auto y) { auto h = eve::pow_abs(eve::lpnorm(p, x, y), eve::oneminus(p)); return eve::pow_abs(x, eve::dec(p))*h; };
+  auto dlpnorm3 = [&](auto p, auto x, auto y) { auto h = eve::pow_abs(eve::lpnorm(p, x, y), eve::oneminus(p)); return eve::pow_abs(y, eve::dec(p))*h; };
 
-  auto dlpnorm1 = [&](auto p, auto x, auto y) { auto h = eve::pow_abs(eve::lpnorm(p, x, y), eve::oneminus(p)); return eve::pow_abs(x, eve::dec(p))*h; };
-  auto dlpnorm2 = [&](auto p, auto x, auto y) { auto h = eve::pow_abs(eve::lpnorm(p, x, y), eve::oneminus(p)); return eve::pow_abs(y, eve::dec(p))*h; };
-  TTS_ULP_EQUAL(flx::derivative_1st(eve::lpnorm)(rp, a0, a1), map(dlpnorm1, rp, a0, a1), 20.0);
+  TTS_RELATIVE_EQUAL(flx::derivative_1st(eve::lpnorm)(rp, a0, a1), map(dlpnorm1, rp, a0, a1), 1.0e-4);
   TTS_ULP_EQUAL(flx::derivative_2nd(eve::lpnorm)(rp, a0, a1), map(dlpnorm2, rp, a0, a1), 20.0);
+  TTS_ULP_EQUAL(flx::derivative_3rd(eve::lpnorm)(rp, a0, a1), map(dlpnorm3, rp, a0, a1), 20.0);
 };
