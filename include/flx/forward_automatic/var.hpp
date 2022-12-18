941edf03476b4f181957663d6addeb3edabaa010
Joel Falcou <joel.falcou@lri.fr>
2022-12-18 18:38:29 +0100

    Augment var() semantic

--- include/flx/forward_automatic/var.hpp
+++ include/flx/forward_automatic/var.hpp
@@ -51,19 +51,27 @@ namespace flx
   struct var_
   {
     template<eve::value T >
-    constexpr auto operator()(T const & x) const noexcept
+    constexpr auto operator()(T x) const noexcept
+    requires(flx::is_valder_v<T>)
+    {
+      return x;
+    }
+
+    template<eve::value T >
+    constexpr auto operator()(T x) const noexcept
     requires(!flx::is_valder_v<T>)
     {
       using vd_t = flx::as_valder_t<T>;
       return vd_t{x, eve::one(eve::as<T>())};
     }
 
-    template < eve::value T >
-    constexpr auto operator()( const T & x, const T & dx) const  noexcept
-    requires(!flx::is_valder_v<T>)
+    template<eve::value T, eve::value U>
+    constexpr auto operator()( T x, U dx) const  noexcept
+    requires(!flx::is_valder_v<T> && !flx::is_valder_v<U>)
     {
       using vd_t = flx::as_valder_t<T>;
-      return vd_t{x, dx};
+      using in_t = eve::as_wide_as_t<T,U>;
+      return vd_t{in_t{x}, in_t{dx}};
     }
   };
 
