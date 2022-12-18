dd23119ab1b360501e69038f8ce467bf1dfd0ba8
Joel Falcou <joel.falcou@lri.fr>
2022-12-18 18:38:55 +0100

    Add proper concept for valder like entities

--- include/flx/forward_automatic/traits.hpp
+++ include/flx/forward_automatic/traits.hpp
@@ -7,14 +7,13 @@
 //==================================================================================================
 #pragma once
 #include <eve/concept/value.hpp>
+#include <eve/product_type.hpp>
 #include <eve/traits.hpp>
 #include <flx/forward_automatic/traits.hpp>
-
 
 namespace flx
 {
   template<eve::scalar_value Type> struct valder;
-
   template<typename T> struct as_valder;
 
   template<eve::scalar_value T> struct as_valder<T>
@@ -27,7 +26,7 @@ namespace flx
     using type = valder<T>;
   };
 
-  template<eve::value T> struct as_valder<T>
+  template<eve::simd_value T> struct as_valder<T>
   {
     using type = eve::wide<valder<eve::element_type_t<T>>, eve::cardinal_t<T>>;
   };
@@ -35,8 +34,8 @@ namespace flx
   template<typename T>
   using as_valder_t = typename as_valder<T>::type;
 
-  template<typename T>              struct as_val;
-  template<eve::value T>                 struct as_val<T>          { using type = T; };
+  template<typename T>          struct as_val;
+  template<eve::value T>        struct as_val<T>          { using type = T; };
   template<eve::scalar_value T> struct as_val<valder<T>>  { using type = T; };
   template<eve::scalar_value T, typename N>
   struct as_val<eve::wide<valder<T>,N>>  { using type = eve::wide<T,N>; };
@@ -44,12 +43,18 @@ namespace flx
   template<typename T>
   using as_val_t = typename as_val<T>::type;
 
-  template<typename T> struct is_valder                   : std::false_type {};
-  template<typename T> struct is_valder<valder<T>>        : std::true_type {};
-  template<typename T> struct is_valder<eve::wide<valder<T>>>  : std::true_type {};
+  template<typename T> struct is_valder                                     : std::false_type {};
+  template<typename T> struct is_valder<valder<T>>                          : std::true_type  {};
+  template<typename T,typename N> struct is_valder<eve::wide<valder<T>,N>>  : std::true_type  {};
 
   template<typename T>
   inline constexpr bool is_valder_v = is_valder<T>::value;
+
+  template<typename T>
+  concept derivate = is_valder_v<T>;
+
+  template<typename T>
+  concept pure = !is_valder_v<T>;
 }
 
 namespace eve
@@ -57,12 +62,15 @@ namespace eve
   template<typename T>
   struct as_logical<flx::valder<T>> : eve::as_logical_t<T>
   {};
+
   template<typename T, typename Sign>
   struct as_integer<flx::valder<T>, Sign> : as_integer_t<T,  Sign>
   {};
+
   template<typename T>
   struct as_integer<flx::valder<T>> : as_integer_t<T>
   {};
+
   template<typename T>
   struct as_uinteger<flx::valder<T>> : as_uinteger_t<T>
   {};
