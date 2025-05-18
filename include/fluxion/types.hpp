89608bf8e8a8b58dbff443e08ca4c4f181ecfa38
jtlap <jtlapreste@gmail.com>
2025-05-18 09:36:06 +0200

    variables

--- include/fluxion/types.hpp
+++ include/fluxion/types.hpp
@@ -11,5 +11,7 @@
 //! @defgroup types Types
 //! @brief Classes and structs defining the hyperdual types or order 1 to 4.
 //======================================================================================================================
+#include <fluxion/details/kumi_utils.hpp>
 #include <fluxion/types/dual_types.hpp>
 #include <fluxion/types/to_dual_types.hpp>
+#include <fluxion/types/variables.hpp>
