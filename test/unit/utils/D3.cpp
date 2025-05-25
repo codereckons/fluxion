2d69ac9f2f740e50adeaf9deb6f441f7572b40b0
Joel Falcou <joel.falcou@lri.fr>
2025-05-25 12:27:51 +0200

    More fix for {}

--- test/unit/utils/D3.cpp
+++ test/unit/utils/D3.cpp
@@ -19,16 +19,16 @@ TTS_CASE_WITH ( "Check flx::D3 over base"
 
   auto f = [](auto x, auto y, auto z){return x*x*y*y*z*z+x*z*z*y; };
 
-  TTS_EQUAL((flx::D<flx::vars{0, 1}>{}(f))(x, y, z), 4*x*y*z*z+z*z);
-  TTS_EQUAL((flx::D<flx::vars{0, 0}>{}(f))(x, y, z), 2*y*y*z*z);
+  TTS_EQUAL((flx::D<flx::vars{0, 1}>(f))(x, y, z), 4*x*y*z*z+z*z);
+  TTS_EQUAL((flx::D<flx::vars{0, 0}>(f))(x, y, z), 2*y*y*z*z);
 
-  TTS_EQUAL((flx::D<flx::vars{0, 0, 0}>{}(f))(x, y, z), T(0));
-  TTS_EQUAL((flx::D<flx::vars{0, 1, 2}>{}(f))(x, y, z), 8*x*y*z+2*z);
+  TTS_EQUAL((flx::D<flx::vars{0, 0, 0}>(f))(x, y, z), T(0));
+  TTS_EQUAL((flx::D<flx::vars{0, 1, 2}>(f))(x, y, z), 8*x*y*z+2*z);
 
-  TTS_EQUAL((flx::D<flx::vars{0, 0, 2}>{}(f))(x, y, z), 4*y*y*z);
-  TTS_ULP_EQUAL((flx::D<flx::vars{0, 2, 2}>{}(f))(x, y, z), 4*x*y*y, 1.0);
-  TTS_ULP_EQUAL((flx::D<flx::vars{0, 1, 1}>{}(f))(x, y, z), 4*x*z*z, 1.0);
+  TTS_EQUAL((flx::D<flx::vars{0, 0, 2}>(f))(x, y, z), 4*y*y*z);
+  TTS_ULP_EQUAL((flx::D<flx::vars{0, 2, 2}>(f))(x, y, z), 4*x*y*y, 1.0);
+  TTS_ULP_EQUAL((flx::D<flx::vars{0, 1, 1}>(f))(x, y, z), 4*x*z*z, 1.0);
 
   auto g = [](auto x){return x*x*x*x; };
-  TTS_ULP_EQUAL((flx::D<flx::vars{0, 0}>{}(g))(x), 12*x*x, 1.0);
+  TTS_ULP_EQUAL((flx::D<flx::vars{0, 0}>(g))(x), 12*x*x, 1.0);
 };
