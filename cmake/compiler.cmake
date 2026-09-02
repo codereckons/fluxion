c6de9eb1b74b8cd35fab8e9ea3a41b9713c0bfd3
Joel Falcou <joel.falcou@lri.fr>
2026-09-02 17:31:46 +0200

    Lay out the CMake the way the hooks want it

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -16,13 +16,13 @@ if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     target_compile_options(fluxion_tests INTERFACE $<$<COMPILE_LANGUAGE:CXX>:/W3 /EHsc>)
   else()
     target_compile_options(fluxion_tests INTERFACE $<$<COMPILE_LANGUAGE:CXX>:-Werror -Wall -Wextra -Wunused-variable
-                                                  -Wshadow -Wdocumentation>)
+                                                   -Wshadow -Wdocumentation>)
   endif()
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
   target_compile_options(fluxion_tests INTERFACE $<$<COMPILE_LANGUAGE:CXX>:/W3 /EHsc /Zc:preprocessor>)
 else()
   target_compile_options(fluxion_tests INTERFACE $<$<COMPILE_LANGUAGE:CXX>:-Werror -Wall -Wextra -Wunused-variable
-                                                -Wshadow>)
+                                                 -Wshadow>)
 endif()
 
 target_include_directories(fluxion_tests INTERFACE ${PROJECT_SOURCE_DIR}/test ${PROJECT_SOURCE_DIR}/include)
